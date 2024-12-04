0cae4e2991cc3d55ff1746792d7c1963009718fd
rr69sport <rrdev.me@gmail.com>
2024-12-04 15:01:09 -0300

    Install lightningcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,23 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    build: {
+      cssMinify: 'lightningcss',
+      assetsInlineLimit: 0
+    },
+    css: {
+      transformer: 'lightningcss',
+      lightningcss: {
+        drafts: {
+          customMedia: true
+        }
+      }
+    }
+  },
+  scopedStyleStrategy: 'where',
+  build: {
+    inlineStylesheets: 'never'
+  }
+});
