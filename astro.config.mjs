2ce757bc521cca32bd057e3056a7d5251b9c2d4a
rr69sport <rrdev.me@gmail.com>
2025-08-23 04:49:33 -0300

    Uninstall "lightningcss" and use "postCSS" with "cssnano" and "autoprefixer"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,24 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     build: {
-      cssMinify: 'lightningcss',
       assetsInlineLimit: 0
-    },
-    css: {
-      transformer: 'lightningcss',
-      lightningcss: {
-        drafts: {
-          customMedia: true
-        }
-      }
     }
   },
   scopedStyleStrategy: 'where',
   build: {
     inlineStylesheets: 'never'
   }
-});
+})
